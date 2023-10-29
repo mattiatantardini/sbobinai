cde8d315db1df5dd4457a8f93750d43efea9e4bb
Mattia Tantardini <mattia.tantardini@gmail.com>
2023-10-29 16:54:31 +0100

    fix(upload): allowing m4a extension

--- app/tabs/upload.py
+++ app/tabs/upload.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 def upload():
 
-    audios = st.file_uploader("Carica gli audio", type=["mp3", "mp4", "wav"], accept_multiple_files=True)
+    audios = st.file_uploader("Carica gli audio", type=["mp3", "mp4", "wav", "m4a"], accept_multiple_files=True)
     transcribe = st.button("Trascrivi")
 
     if transcribe:
